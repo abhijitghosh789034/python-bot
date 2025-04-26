5139a8a60ee361691bf90cc543c634396b482e7f
abhijitghosh789034 <abhijitghosh789034@gmail.com>
2025-04-26 07:15:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import telebot
 ##TOKEN DETAILS
 TOKEN = "TRON"
 
-BOT_TOKEN = "5710284858:AAHcIDYAtWAC01p8BsHRl4cIwhcKpBqNlTQ"
-PAYMENT_CHANNEL = "@testpostchnl" #add payment channel here including the '@' sign
-OWNER_ID = 5151868182 #write owner's user id here.. get it from @MissRose_Bot by /id
+BOT_TOKEN = "7963574219:AAGd7V2HR2pcpQW_ND1Pgsyzcfi8IyKBPok"
+PAYMENT_CHANNEL = "https://t.me/earnwithjaddubot" #add payment channel here including the '@' sign
+OWNER_ID = 7859280271 #write owner's user id here.. get it from @MissRose_Bot by /id
 CHANNELS = ["@testpostchnl"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 1 #Put daily bonus amount here!
